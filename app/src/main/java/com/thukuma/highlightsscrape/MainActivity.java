84c1aa2399ae1498cb3ff9d739adb7547a2c5300
Huawei <hninwaithaw65@gmail.com>
2022-12-10 11:41:05 +0630

    NEW LIB

--- app/src/main/java/com/thukuma/highlightsscrape/MainActivity.java
+++ app/src/main/java/com/thukuma/highlightsscrape/MainActivity.java
@@ -17,28 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Highlight highlight = new Highlight(this, "https://www.fasthighlights.net/tag/home", new Highlight.onComplete() {
-//            @Override
-//            public void onComplete(HighlightModal modal) {
-//                Log.d("my-test", "onComplete: "+modal.getHref());
-//            }
-//        }, new Highlight.onError() {
-//            @Override
-//            public void onError(Exception e) {
-//
-//            }
-//        });
-        new Stream(this, "https://www.fasthighlights.net/2022/10/bologna-vs-lecce-highlights/", new Stream.onComplete() {
+        Highlight highlight = new Highlight(this, "https://www.fasthighlights.net/tag/home", new Highlight.onComplete() {
             @Override
-            public void onComplete(StreamModal modal) {
-                Log.d("my-test", "onComplete: "+modal.getLink());
+            public void onComplete(HighlightModal modal) {
+                Log.d("my-test", "onComplete: "+modal.getHref());
+                Log.d("my-test", "onComplete: "+modal.getDate());
+                Log.d("my-test", "onComplete: "+modal.getImage());
+                Log.d("my-test", "onComplete: "+modal.getLeague());
+                Log.d("my-test", "onComplete: "+modal.getTitle());
             }
-        }, new Stream.onError() {
+        }, new Highlight.onError() {
             @Override
             public void onError(Exception e) {
 
             }
         });
+//        new Stream(this, "https://www.fasthighlights.net/2022/10/bologna-vs-lecce-highlights/", new Stream.onComplete() {
+//            @Override
+//            public void onComplete(StreamModal modal) {
+//                Log.d("my-test", "onComplete: "+modal.getLink());
+//            }
+//        }, new Stream.onError() {
+//            @Override
+//            public void onError(Exception e) {
+//
+//            }
+//        });
 
     }
 }
